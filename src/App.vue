79f6cee5f1cfea1f4975c44daf162cbac27ebe8c
sillhero <1296928944@qq.com>
2023-06-03 12:58:43 +0800

    第二种绑定事件
    第二种绑定事件

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <h1>我是App组件</h1>
     <h3 v-show="toy">儿子很孝顺给我游戏机:{{toy}}</h3>
     <!-- 这里的send-toy是监听事件 -->
-    <Child @send-toy="getToy"></Child> 
+    <!-- <Child @send-toy="getToy"></Child>  -->
+
+    <Child ref="haizi"/>
   </div>
 </template>
 
@@ -24,6 +26,9 @@
                 console.log("App组件接收到了儿子的礼物", toy);
                 this.toy = toy
             }
+        },
+        mounted() {
+          this.$refs.haizi.$on('send-toy', this.getToy);
         }
     }
 </script>
